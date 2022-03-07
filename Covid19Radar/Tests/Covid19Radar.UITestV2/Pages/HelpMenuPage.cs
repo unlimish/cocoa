d913bb5f833d6bef3dd8b05960f7fcf8221fb6cc
cocoa-test002 <cocoa-test002@shiftinc.jp>
2022-03-07 14:27:08 +0900

    Fix S24 for iOS

--- Covid19Radar/Tests/Covid19Radar.UITestV2/Pages/HelpMenuPage.cs
+++ Covid19Radar/Tests/Covid19Radar.UITestV2/Pages/HelpMenuPage.cs
@@ -50,7 +50,7 @@ namespace CovidRadar.UITestV2
                 openHelpPage3 = x => x.Marked("HelpMenuPageTitle").Class("UILabel").Index(2); //新型コロナウイルスに感染していると判定されたら
                 openHelpPage4 = x => x.Marked("HelpMenuPageTitle").Class("UILabel").Index(3); //接触の記録を停止/情報を削除するには
                 toolBarBack = x => x.Id("BackButton").Class("UIButton").Index(0); //戻るボタン
-                openMenuPage = x => x.Class("UIButton").Index(3);//ハンバーガーメニュー
+                openMenuPage = x => x.Class("UIButton").Index(0);//ハンバーガーメニュー
             }
         }
 

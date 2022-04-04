b902cdba40d5354406a7df4d523a38f163b2ee27
Hiroshi Saito <hiroshi.saito.73@shiftinc.jp>
2022-04-04 10:42:03 +0900

    Revert S13 & add test scenario

--- Covid19Radar/Tests/Covid19Radar.UITestV2/Tests/S13_Tests.cs
+++ Covid19Radar/Tests/Covid19Radar.UITestV2/Tests/S13_Tests.cs
@@ -18,34 +18,27 @@ namespace CovidRadar.UITestV2
         }
 
 
-
-
-
-        public void S13(int count = 2)
+        [Test]
+        public void Case01_Test()
         {
+
             HomePage homePage = new HomePage();
             homePage.AssertHomePage();
-
             //S1 ホーム画面で、ハンバーガーメニューを押下
             MenuPage menuPage = homePage.OpenMenuPage();
             menuPage.AssertMenuPage();
-
             //S2 ハンバーガーメニューで、「設定」ボタンを押下
             SettingsPage settingsPage = menuPage.OpenSettingsPage();
             settingsPage.AssertSettingsPage();
-
             //S3 「設定」画面で、「アプリを初期化」ボタンを押下
             settingsPage.TapSyokika();
             settingsPage.AssertSettingsPage();
-
             //S4 「設定」画面で、「アプリの初期化を行います」ポップアップで「キャンセル」ボタンを押下
             settingsPage.TapDialogCancelBtn();
             settingsPage.AssertSettingsPage();
-
             //S5 「設定」画面で、「アプリを初期化」ボタンを押下
             settingsPage.TapSyokika();
             settingsPage.AssertSettingsPage();
-
             //S6 「設定」画面で、「アプリの初期化を行います」ポップアップで「OK」ボタンを押下
             settingsPage.TaplDialogOKBtn();
             settingsPage.AssertSettingsPage();
@@ -55,33 +48,7 @@ namespace CovidRadar.UITestV2
 
             AppManager.ReStartApp();
             TutorialPage1 tutorialPage1 = new TutorialPage1();
-
-
-            try
-            {
-                tutorialPage1.AssertTutorialPage1();
-            }
-            catch (Exception e)
-            {
-                if (count > 0)
-                {
-                    count--;
-                    S13(count);
-                }
-                else
-                {
-                    throw e;
-                }
-            }
-
-        }
-
-
-
-        [Test]
-        public void Case01_Test()
-        {
-            S13();
+            tutorialPage1.AssertTutorialPage1();
 
         }
 
